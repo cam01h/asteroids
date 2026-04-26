8f14598dfe2096eeed43f35e1551d2cf432ade63
cam01h <cam01houston@gmail.com>
2026-04-26 20:59:24 +0100

    logger and window launch

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
 
 def main():
     print("Starting Asteroids with pygame version: VERSION")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    pygame.display.set_caption("Asteroids")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            log_state()
+            screen.fill("black")
+            pygame.display.flip()
+            if event.type == pygame.QUIT:
+                return
+            pass
 
 
 if __name__ == "__main__":
