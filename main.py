f40a286e7f227db4a8123b102be7c4790962b625
cam01h <cam01houston@gmail.com>
2026-04-26 21:31:55 +0100

    added clock and fps limit to main.py and added circleshape.py with the CircleShape class

--- main.py
+++ main.py
@@ -9,17 +9,20 @@ def main():
     pygame.init()
     pygame.display.set_caption("Asteroids")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    running = True
+    clock = pygame.time.Clock()
+    dt = 0
 
+    running = True
     while running:
         for event in pygame.event.get():
-            log_state()
-            screen.fill("black")
-            pygame.display.flip()
             if event.type == pygame.QUIT:
                 return
             pass
 
+        log_state()
+        screen.fill("black")
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
